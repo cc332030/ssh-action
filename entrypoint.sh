7de5b4e19899c79b1dd785cfb98bc5554a863987
c332030 <c332030@gmail.com>
2022-07-13 14:29:04 +0800

    feat: ssh-action

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+parameter=$*
+
+curl_sh(){
+  sh -c "curl -sL https://github.com/cc332030$1 | sh -s -- $parameter"
+}
+
+curl_sh /linux/raw/master/script/init-ssh/init-ssh-pre.sh
+curl_sh /linux/raw/master/script/init-ssh/init-ssh.sh
+
+if [ -n "$COMMAND" ]
+then
+  sh -c "${COMMAND}" || true
+fi
